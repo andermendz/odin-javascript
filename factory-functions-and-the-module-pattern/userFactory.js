fb6a94d5d66e223f994a53e79da1fc30c91ee59b
andermendz <andersonmendz19@gmail.com>
2024-01-21 20:17:10 -0500

    added boilerplate code for classes

--- factory-functions-and-the-module-pattern/userFactory.js
+++ factory-functions-and-the-module-pattern/userFactory.js
@@ -42,3 +42,16 @@ const andermendz = createPlayer('andermendz','10');
 andermendz.increaseLevel();
 
 console.log('Your gamer level is: ' + andermendz.getLevel())
+
+
+const calculator = (function () {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return { add, sub, mul, div };
+  })();
+  
+  calculator.add(3,5); // 8
+  calculator.sub(6,2); // 4
+  calculator.mul(14,5534); // 77476
